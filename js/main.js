4899b1cc9a9d9561ebb623361e1969d5abf9143d
Clickandgo informatique <mail@clickandgo-informatique.com>
2022-12-21 01:21:23 +0100

    Ajout de logique code + css (pour essai)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 const cube = document.getElementById('cube')
+const btnRoll = document.getElementById('btn-roll')
+const btnNewGame = document.getElementById('btn-new-game')
+const joueur1 = document.getElementById('joueur1')
+const joueur2 = document.getElementById('joueur2')
+const divScoreCourantJ1 = document.querySelector('#score-courant-joueur1 h4')
+const divScoreTotalJ1 = document.querySelector('#score-total-joueur1 h4')
+// const scoreTotalJ1 = null
+console.log(divScoreTotalJ1)
+const pointsJ1 = []
+const pointsJ2 = []
+
 angleArray = [
     [0, 0, 0],
     [-310, -362, -38],
@@ -9,36 +20,46 @@ angleArray = [
     [-133, -360, -53]
 ];
 
-cube.addEventListener('click', (e) => {
-    const randomAngle = Math.floor(Math.random() * 6) + 1
-   const resultatTirage=(randomAngle)=>{
+const premierJoueur = (joueur) => {
 
-       switch(randomAngle){
-           case 1:            
-               return "Vous avez tiré le chiffre un !"
-           case 2:
-               return "Vous avez tiré le chiffre deux !"
-               
-           case 3:
-               return "Vous avez tiré le chiffre trois !"
-               
-           case 4:
-               return "Vous avez tiré le chiffre quatre !"
-               
-           case 5:
-               return "Vous avez tiré le chiffre cinq !"
-               
-           case 6:
-               return "Vous avez tiré le chiffre six !"
-               
-               default:
-           return "Veuillez tirer le dè"
-       }
-   } 
-   console.log(resultatTirage)
+    joueur = Math.floor(Math.random() * 2 + 1)
+    console.log(`Joueur ${joueur} commence.`)
+    return joueur
+}
+
+btnRoll.addEventListener('click', () => {
+    const randomAngle = Math.floor(Math.random() * 6) + 1
+    const resultatTirage = (randomAngle) => {
+
+        switch (randomAngle) {
+            case 1:
+                return "Vous avez tiré le chiffre un !"
+            case 2:
+                return "Vous avez tiré le chiffre deux !"
+            case 3:
+                return "Vous avez tiré le chiffre trois !"
+            case 4:
+                return "Vous avez tiré le chiffre quatre !"
+            case 5:
+                return "Vous avez tiré le chiffre cinq !"
+            case 6:
+                return "Vous avez tiré le chiffre six !"
+            default:
+                return "Veuillez tirer le dè"
+        }
+    }
+    //Remplir tableau de score courant du joueur actif
+    pointsJ1.push(randomAngle)
+    //Calcul du total
+    const scoreTotalJ1 = pointsJ1.reduce((a, b) => a + b)
+    divScoreTotalJ1.innerHTML = scoreTotalJ1
+    divScoreCourantJ1.innerHTML=randomAngle
+
+    console.log(pointsJ1)
+    console.log(scoreTotalJ1)
 
     /*ANIMATION */
-    cube.style.animation = 'animate 4s linear';
+    cube.style.animation = 'animate 1.4s linear';
     //console.log(randomAngle);
     cube.style.transform = 'rotateX(' + angleArray[randomAngle][0] + 'deg) rotateY(' + angleArray[randomAngle][1] + 'deg) rotateZ(' + angleArray[randomAngle][2] + 'deg)';
     cube.style.transition = '1s linear'
@@ -46,4 +67,34 @@ cube.addEventListener('click', (e) => {
     cube.addEventListener('animationend', function (e) {
         cube.style.animation = '';
     });
+})
+
+//Nouvelle partie
+btnNewGame.addEventListener('click', () => {
+    //Mise à zéro des champs et tableaux
+    pointsJ1 = []
+    pointsJ2 = []
+
+    //Remise à zéro de la bordure pour les deux champs
+    if (joueur1.classList.contains("selection")) {
+        joueur1.classList.remove("selection")
+    }
+    if (joueur2.classList.contains("selection")) {
+        joueur2.classList.remove("selection")
+    }
+
+    //Tirage au sort du joueur débutant la partie (fonction: "premierJoueur")
+    let joueurActif = premierJoueur()
+    //Application d'une bordure distinctive au joueur actif
+    switch (joueurActif) {
+        case 1:
+            joueur1.classList.add("selection")
+            joueur2.classList.remove("selection")
+        case 2:
+            joueur2.classList.add("selection")
+            joueur1.classList.remove("selection")
+        default:
+            console.log("joueur actif = " + joueurActif)
+            return
+    }
 })
