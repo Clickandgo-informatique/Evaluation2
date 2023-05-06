f8bdef9f715c279fb12478c0b4008f496f483177
Clickandgo informatique <mail@clickandgo-informatique.com>
2023-05-06 13:54:10 +0200

    essai suite blocage Heroku

---
+++ includes/container-joueur-actif.php
@@ -0,0 +1,9 @@
+                     <div class="container container-joueur-actif">
+                      <div class="row">
+                          <div class="col-md-10 mx-auto p-1">
+                              <div class="joueurActif text-Shadow text-center">
+                                  <p class="text">Aucun joueur actif</p>
+                              </div>
+                          </div>
+                      </div>
+                  </div><!-- Fin container-joueur-actif -->
